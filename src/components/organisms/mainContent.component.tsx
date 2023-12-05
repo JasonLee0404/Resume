82e06254820b2ac551db6075f3575d6d312c67e1
Jason Le <jason.le@consilium.technology>
2023-12-05 15:08:22 +1030

    added image and ability to modify content

--- src/components/organisms/mainContent.component.tsx
+++ src/components/organisms/mainContent.component.tsx
@@ -1,4 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Card, CardBody, Text, Image } from "@chakra-ui/react";
+import { content } from "../../content/content";
+import aboutMe from "../../assets/aboutMe.jpg"
 
 interface MainContentProps {
     tabName?: string
@@ -6,10 +8,36 @@ interface MainContentProps {
 
 const MainContent: React.FC<MainContentProps> = ( { tabName} ) => {
 
+    let mainContent; 
+
+    if (tabName === 'About Page') {
+        mainContent = 
+        
+        <>
+            <Card>
+                
+                <CardBody>
+                    
+                    <Text as='b' fontSize='2xl'>{content.aboutMe.heading}</Text>
+                    <Text>{content.aboutMe.detail}</Text>
+                    <Box boxSize='sm' mt='1rem'>
+                        <Image src={aboutMe} borderRadius='lg'/>
+                    </Box>
+                </CardBody>
+            </Card>
+        </>
+    } else if (tabName === 'Project Page') {
+        mainContent = 
+        <>
+            BBB
+        </>
+    }
+
+
     return (
         <>
             <Box flex='1' bg='white' w='100%' backgroundColor='gray.50' padding='2'>
-                This is main content page for {tabName}
+                {mainContent}
             </Box>
         </>
     );
---
+++ src/content/content.ts
@@ -0,0 +1,7 @@
+export const content = {
+    aboutMe: {
+        heading: 'About Me',
+        detail: 'An experienced full-stack software engineer with skills in both front-end and back-end development, as well integrations, software architecture and the full software development lifecycle. Apart from software engineering, he also possess skills in project management, product management, UI/UX design and other non-engineering discipline such as economics, finance, business. He can work well as part of a cross-functional team, following the full software development lifecycle, or work on his own in technical investigation and prototyping projects. He has a passion for problem solving, regardless of the domain, and has pride in bringing high quality software solutions to clients.'
+    },
+
+}
